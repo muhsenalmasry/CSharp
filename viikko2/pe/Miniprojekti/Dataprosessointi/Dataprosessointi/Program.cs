4e3b9ff0dd778182ae73b88191b9d30136197d2f
Muhsen Almasry <muhsen.almasry94@gmail.com>
2021-07-02 16:16:16 +0300

    Esittelyä varten

--- viikko2/pe/Miniprojekti/Dataprosessointi/Dataprosessointi/Program.cs
+++ viikko2/pe/Miniprojekti/Dataprosessointi/Dataprosessointi/Program.cs
@@ -13,9 +13,12 @@ namespace Dataprosessointi
             foreach (var rivi in File.ReadAllLines(@"C:\users\muhsen\desktop\CourseData\CourseData.csv"))
             {
                 var data = rivi.Split(";");
+                Console.WriteLine(data[0]);
                 lista.Add(new Data(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]));
             }
-
+            lista.RemoveAt(0);
+            Data.VirheellisetRivit(lista);
+            Data.TulostusMuoto(lista);
         }
     }
 }
